4dc64e727e4fad0ecf120ca7f34357733b189c3d
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-05-23 20:50:14 +0300

    11

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Children, createElement } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "components/App";
 import "./index.css";
